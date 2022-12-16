6f0fdcfeec7c520e4a8b46f01b03062b5acbd2d5
Đạt <94163649+datnguyendevfrontend@users.noreply.github.com>
2022-12-16 16:14:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+// Cách 1 kiểu bình thường
+function One(){
+  return (
+    <div> 
+       <h2>cách dùng thứ 1 kiểu bình thường</h2>
+    </div>
+  )
+}
+
+// Cách 2 dùng kiểu ân danh
+var Two = function (){
+	return(
+		<div>
+			<h2> cách dùng thứ 2 kiểu ẩn danh</h2>
+		</div>
+	);
+}
+
+// kiểu này thường sử dụng
+var Three = () =>(
+  <div> 
+        <h3>cách dùng thứ 3 arrow function</h3>
+  </div>
+);
+
+
+// kiểu class
+class Four extends Component {
+  render() {
+    return (
+      <div>
+        <h1>texxt</h1>
+      </div>
+    );
+  }
+};
+
 
 function App() {
   return (
@@ -9,6 +48,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <One/>
+        <Two/>
+        <Three/>
+        <Four/>
         <a
           className="App-link"
           href="https://reactjs.org"
